470f14be84de2142153fa0e7abf9ebab686e2c3d
JayAllen00 <99896718+JayAllen00@users.noreply.github.com>
2022-08-28 18:24:37 -0400

    Eighth Commit

---
+++ Python/PythonBillsMafiaScript2/main.py
@@ -0,0 +1,6 @@
+phrase = "Bills Mafia"
+fans = "Fanbase"
+print("Welcome to " + phrase + ".")
+print("We are the best " + fans + " in the world.")
+print("The Bufflo Bills love their " + fans + "," )
+print("The Buffalo Bills love " + phrase + ".")
