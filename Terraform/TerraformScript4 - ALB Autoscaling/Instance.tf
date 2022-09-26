edef4a457bced145cb0081f245a99d0ea7731c20
JayAllen00 <99896718+JayAllen00@users.noreply.github.com>
2022-09-25 21:12:55 -0400

    Eleventh Commit

--- Terraform/TerraformScript4 - ALB Autoscaling/Instance.tf
+++ Terraform/TerraformScript4 - ALB Autoscaling/Instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "instance1" {
-  ami           = "ami-0f924dc71d44d23e2"
+  ami           = "ami-from us-east-2"
   instance_type = "t2.micro"
   security_groups = ["${aws_security_group.myinstance.id}"]
   subnet_id = "${aws_subnet.main-private-1.id}"
